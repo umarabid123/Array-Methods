c98a5151ee10b6fc611fa300aeb6473642b687d7
umarabid123 <umarabid709@gmail.com>
2024-05-17 15:21:51 +0500

    first commit

---
+++ Array.js
@@ -0,0 +1,90 @@
+// Array Methods
+
+// check the valu by using index 
+//  let arr = [1,2,3,4,5,6,9];
+//  console.log(arr.at(4));
+//  console.log(arr.at(6));
+
+// Add value in array at the end of the array 
+//  let arr = [1,2,3,4,5,6,9];
+// arr.push(10)
+// console.log(arr);
+
+
+// Remove the element at last index of the array
+//  let arr = [1,2,3,4,5,6,9];
+// arr.pop()
+// console.log(arr);
+
+
+// Fill the same element in the array 
+//  let arr = [1,2,3,4,5,6,9];
+//  arr.fill(10);
+//  console.log(arr);
+
+
+// Remove the first element of the array
+// let arr = [12, 13, 17, 19, 20];
+// arr.shift();
+// console.log(arr);
+
+// Add Element at the first index of the array
+// let arr = [12, 13, 17, 19, 20];
+// arr.unshift(11);
+// console.log(arr);
+
+// Reverse in Decending or ascending order  order to sorted array
+
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+// let arr =[1, 2, 3, 4, 5, 6,7,8,9,];
+// arr.reverse()
+// console.log(arr);
+
+// Check the element is include or exist in the array
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+// arr2=arr.includes(2);
+// console.log(arr2);
+
+// change the values of the array and store in new variable
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+//     var arr2 =arr.map(number => 2 *number);
+// console.log(arr2);
+
+
+// Filter the specific value in the array
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+// let arr2 =arr.filter(number => number > 5);
+// console.log(arr2);
+
+//  Use of reduce method sum the  multiple in one unit 
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+// let arr2 = arr.reduce((accum, curr) => {
+// // return accum + curr;
+// // return accum * curr;
+
+// },7)
+// console.log(arr2);
+
+// change the integers into string
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+// let arr2 = arr.toString();
+// console.log(typeof arr2)
+// console.log(arr2);
+
+//  join all elemnts of array
+// let arr = ['Muhammad', 'umar', 'Ahmad']
+// let arr2 = arr.join('.');
+// console.log(arr2);
+// console.log(typeof arr2);
+
+// Add elements in the array
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+// arr.splice(3,0, 'five', 'six');
+// console.log(arr);
+
+// Store the slice of array specific index in new array
+// let arr =[9, 8, 7, 6, 5, 4, 3, 2, 1];
+// let arr2=arr.slice(4,6);
+// console.log(arr2);
+
+// 
