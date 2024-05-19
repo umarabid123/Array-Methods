28faa9ef9aac71332c926d203cd7f3c9a72fe236
umarabid123 <umarabid709@gmail.com>
2024-05-19 14:08:03 +0500

    changes

---
+++ Number.js
@@ -0,0 +1,25 @@
+// Number Methods Practice
+
+// let num = 5.4748;
+// console.log(num.toFixed(0)); 
+// let num = 5.4748;
+// console.log(num.toFixed(2));
+
+// set the numbers of values after decimal point
+// let num = 5.4748;
+// console.log(num.toPrecision(3));
+// console.log(num.toPrecision(4));
+// console.log(num.toPrecision(1));
+
+// reurn value of variable
+// let num = 5.4748;
+// console.log(num.valueOf());
+
+// Change sring to integer and return only integ value
+
+// let day = "10 day";
+// console.log(parseInt(day));
+
+// string to float 
+let day = "10.5 day";
+console.log(parseFloat(day));
